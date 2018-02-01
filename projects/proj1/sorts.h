321419486fed7e6359c54bf2bbc5a346fb65ccc6
Dane Johnson <dane_johnson@ymail.com>
2018-02-01 10:37:51 -0600

    Finished ll mergesort

--- projects/proj1/sorts.h
+++ projects/proj1/sorts.h
@@ -10,4 +10,6 @@ void quicksort_list(llnode *list, llnode *start, llnode *tail);
 //////////////////// MERGESORT ////////////////////
 void mergesort_array(int arr[], int n);
 void merge_array(int arr[], int arr1[], int arr2[], int n1, int n2);
+void mergesort_list(llnode **list);
+void merge_list(llnode **list, llnode *list1, llnode *list2)
 #endif // SORTS_H_
