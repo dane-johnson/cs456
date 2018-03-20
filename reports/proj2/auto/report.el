ed0537ec13f6f384ecfb594c8f5c48f9133a1c4e
Dane Johnson <dane_johnson@ymail.com>
2018-03-20 15:30:00 -0500

    Some work done on report, trial csvs in

---
+++ reports/proj2/auto/report.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "12pt")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "graphicx"
+    "fancyhdr"
+    "listings"))
+ :latex)
+
