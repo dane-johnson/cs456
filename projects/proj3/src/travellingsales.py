54c200532209bdd8f936835b6a53673fa38fb8d1
Dane Johnson <daneallenjohnson@protonmail.com>
2018-04-05 09:28:06 -0500

    Added bnb

--- projects/proj3/src/travellingsales.py
+++ projects/proj3/src/travellingsales.py
@@ -1,4 +1,7 @@
 from math import sqrt
+
+from shortestpath import MinPriorityHeap
+
 def dist_sqrd(p1, p2):
   """Calculate distance between two points squared"""
   x1, y1 = p1
@@ -35,9 +38,38 @@ def permute(points):
 
 def brute_force_ts(points):
   """Slow, try-everything approach"""
-  configurations = permute(points)
+  ## Doesn't matter where we start, pick any point
+  src = points[0]
+  configurations = map(lambda x: [src] + x, permute(points[1:]))
   best = min(configurations, key=score)
   return best, proper_score(best)
+
+def bnb_ts(points):
+  """More enlightened approach, only search promising trees"""
+  ## Doesn't matter where we start, pick any point
+  state = {
+    "placed": points[:1]
+    "remaining": points[1:]
+    "lower_bound": lower_bound(points[:1], points[1:])
+  }
+  solution = None
+  queue = MinPriorityHeap()
+  queue.insert(state, state['lower_bound'])
+  while len(queue) != 0:
+    curr = queue.extract_min
+    if len(curr['remaining']) == 1:
+      ## We have found a solution
+      candidates = calc_next_states(curr)
+      if solution:
+        candidates.append(solution)
+      solution = min(candidates, lamda x: x['lower_bound'])
+      queue.prune(x['lower_bound'])
+    else:
+      ## Calculate the next states and add them to the queue
+      next_states = calc_next_states(curr)
+      for state in next_states:
+        queue.insert(state, state['lower_bound'])
+  return solution
 
 def read_file(filename):
   """Reads in an input file into a list of points"""
