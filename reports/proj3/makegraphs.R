d2e42ba2b33b3766be5deb8aed386cd0687e7115
Dane Johnson <daneallenjohnson@protonmail.com>
2018-04-16 10:54:09 -0500

    WIP graphs

---
+++ reports/proj3/makegraphs.R
@@ -0,0 +1,11 @@
+## Read in the data
+data <- read.csv(file="runs.csv", header=TRUE, sep=',')
+
+for (set in c("BruteForce", "BranchAndBound", "DynamicProgramming", "MSTApproximation")) {
+    algodata = data[data$ALGO == set,]
+    pdf(paste(set, ".pdf", sep=""))
+    plot(algodata$N, algodata$TIME, xlab="Time (seconds)", ylab="Number of Nodes")
+    abline(lm(algodata$TIME ~ algodata$N + 0))
+    dev.off()
+}
+
