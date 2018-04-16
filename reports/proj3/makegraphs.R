730939c2bf6741b842ca0acf9acc7d27a6363c1f
Dane Johnson <daneallenjohnson@protonmail.com>
2018-04-16 12:44:52 -0500

    Corrected axes

--- reports/proj3/makegraphs.R
+++ reports/proj3/makegraphs.R
@@ -4,6 +4,6 @@ data <- read.csv(file="runs.csv", header=TRUE, sep=',')
 for (set in c("BruteForce", "BranchAndBound", "DynamicProgramming", "MSTApproximation")) {
     algodata = data[data$ALGO == set,]
     pdf(paste(set, ".pdf", sep=""))
-    plot(algodata$N, algodata$TIME, xlab="Time (seconds)", ylab="Number of Nodes")
+    plot(algodata$N, algodata$TIME, ylab="Time (seconds)", xlab="Number of Nodes", log="y")
     dev.off()
 }
