af3d4f7be4fc5fd0046a71c18a882c826873f52d
dane-johnson <dane_johnson@ymail.com>
2018-04-15 19:43:19 -0500

    Modified runner

--- projects/proj3/tools/runner.py
+++ projects/proj3/tools/runner.py
@@ -2,6 +2,8 @@
 import random
 from travellingsales import brute_force_ts, branch_and_bound_ts, dynamic_programming_ts, mst_approx_ts
 
+N_MAX = 15
+
 tests = {
   "BruteForce": (brute_force_ts, []),
   "BranchAndBound": (branch_and_bound_ts, []),
@@ -11,7 +13,7 @@ tests = {
 
 random.seed(0)
 
-for i in xrange(2, 21):
+for i in xrange(2, N_MAX + 1):
   graph = []
   for j in xrange(i):
     graph.append((random.randrange(50), random.randrange(50)))
@@ -23,5 +25,5 @@ for i in xrange(2, 21):
 with open('runs.csv', 'w') as fout:
   for test in tests:
     function, scores = tests[test]
-    for i in xrange(2, 11):
+    for i in xrange(2, N_MAX + 1):
       fout.write('%s,%d,%f\n' % (test, i, scores[i-2]))
