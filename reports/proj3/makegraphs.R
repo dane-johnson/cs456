70d88049b2b868a3318350b456bc850d1b850d31
Dane Johnson <dane_johnson@ymail.com>
2018-04-17 12:51:04 -0500

    Added labels to graphs

--- reports/proj3/makegraphs.R
+++ reports/proj3/makegraphs.R
@@ -1,9 +1,11 @@
 ## Read in the data
 data <- read.csv(file="runs.csv", header=TRUE, sep=',')
+library("calibrate")
 
 for (set in c("BruteForce", "BranchAndBound", "DynamicProgramming", "MSTApproximation")) {
     algodata = data[data$ALGO == set,]
     pdf(paste(set, ".pdf", sep=""))
-    plot(algodata$N, algodata$TIME, ylab="Time (seconds)", xlab="Number of Nodes", log="y")
+    plot(algodata$N, algodata$TIME, ylab="Time (seconds)", xlab="Number of Nodes", xlim=c(2,11), log="y")
+    textxy(algodata$N, algodata$TIME, algodata$TIME)
     dev.off()
 }
